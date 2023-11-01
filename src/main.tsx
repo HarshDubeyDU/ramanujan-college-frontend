b4d4b5f3dac0536096f2c4a32db775950eab8833
Harsh Dubey <87745474+HarshDubeyDU@users.noreply.github.com>
2023-11-01 17:34:51 +0530

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { AuthProvider } from "./context/AuthContext.tsx";
-import { QueryProvider } from "./lib/react-query/QueryProvider.tsx";
+import { AuthProvider } from "@/context/AuthContext";
+import { QueryProvider } from "@/lib/react-query/QueryProvider";
 
 import App from "./App";
 
